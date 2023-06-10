6aba023a7cff44979cc2a0d81dd19114e14f0ac9
Smirnova Nina <ms.nina.smirnova@gmail.com>
2023-06-10 22:28:33 +0200

    turn debug ib

--- webapp/settings.py
+++ webapp/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tua01d=bt57&g_j7-nino=h#!-4l8$gy-#s0irszvmemcqsq2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
@@ -162,8 +162,8 @@ STORAGES = {
 }
 
 
-STATIC_HOST = "https://bcp-django.herokuapp.com" if not DEBUG else ""
-STATIC_URL = STATIC_HOST + "/static/"
+# STATIC_HOST = "https://bcp-django.herokuapp.com" if not DEBUG else ""
+# STATIC_URL = STATIC_HOST + "/static/"
 
 
 # Default primary key field type
